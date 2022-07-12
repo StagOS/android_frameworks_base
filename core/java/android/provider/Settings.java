799da7503f19a84e9a4f350ab25d0f34a5f6efff
vjspranav <pranavasri@live.in>
2022-07-13 01:57:15 +0200

    base: Add toggle for custom Settings UI [1/3]
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6088,6 +6088,15 @@ public final class Settings {
          */
         public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6219,6 +6228,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION_COLOR);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
