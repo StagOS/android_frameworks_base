e21004321d4f77111fd7b5bd5353ed9dc207cf26
cjh1249131356 <cjh1249131356@gmail.com>
2022-09-25 14:50:31 +0800

    base: Add mock oplus LinearmotorVibratorService
    * To get haptic feedback in OnePlus / Oplus Camera.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5980,6 +5980,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         VibrationEffect.Composition.PRIMITIVE_LOW_TICK, 0.2f,
                         VibrationEffect.EFFECT_TEXTURE_TICK);
 
+            case HapticFeedbackConstants.GRANULAR_SHORT_VIBRATE:
+                return VibrationEffect.get(VibrationEffect.EFFECT_CLICK);
+
             case HapticFeedbackConstants.NO_HAPTICS:
             default:
                 return null;
