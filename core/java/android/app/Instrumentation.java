PixysOS-Beta/platform_frameworks_base

Introduce Pixel/Games PropUtils [SQUASHED]
* Adapted for android 14
* Removed pixel 6 pro spoof
* block pe feature flags for some apps to get rid of tensor

[1] https://github.com/PixysOS/frameworks_base/commits/cb5e356ab56d1edea014a3f93ca36a9f2004a2bf/core/java/com/android/internal/util/pixys/PixelPropsUtils.java

[2] https://github.com/PixysOS/frameworks_base/commits/cb5e356ab56d1edea014a3f93ca36a9f2004a2bf/core/java/com/android/internal/util/pixys/GamesPropsUtils.java

Co-authored-by: johnmart19 <johnivan19999@gmail.com>
Co-authored-by: aswin7469 <aswinas@pixysos.com>
Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Danny Lin <danny@kdrag0n.dev>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: Dyneteve <dyneteve@hentaios.com>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Change-Id: I466251643fb9d46fd030e545fcbadff8a02cdc39
Signed-off-by: aswin7469 <aswinas@pixysos.com>

core/java/android/app/Instrumentation.java
@@ -59,6 +59,8 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.pixys.PixelPropsUtils;
+import com.android.internal.util.pixys.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1281,6 +1283,9 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1298,6 +1303,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
 

core/java/com/android/internal/util/pixys/GamesPropsUtils.java
@@ -1,0 +1,155 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.pixys;
+
+import android.os.Build;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GamesPropsUtils {
+
+    private static final String TAG = GamesPropsUtils.class.getSimpleName();
+    private static final boolean DEBUG = false;
+
+    private static final Map<String, Object> propsToChangeBS4 = createMap("2SM-X706B", "blackshark");
+    private static final String[] packagesToChangeBS4 = { // spoof as Black Shark 4
+            "com.proximabeta.mf.uamo"
+    };
+
+    private static final Map<String, Object> propsToChangeMI11TP = createMap("2107113SI", "Xiaomi");
+    private static final String[] packagesToChangeMI11TP = { // spoof as Mi 11T PRO
+            "com.ea.gp.apexlegendsmobilefps",
+            "com.levelinfinite.hotta.gp",
+            "com.supercell.clashofclans",
+            "com.vng.mlbbvn",
+            "com.mobile.legends"
+    };
+
+    private static final Map<String, Object> propsToChangeMI13P = createMap("2210132C", "Xiaomi");
+    private static final String[] packagesToChangeMI13P = { // spoof as Mi 13 PRO
+            "com.levelinfinite.sgameGlobal",
+            "com.tencent.tmgp.sgame"
+    };
+
+    private static final Map<String, Object> propsToChangeOP8P = createMap("IN2020", "OnePlus");
+    private static final String[] packagesToChangeOP8P = { // spoof as OnePlus 8 PRO
+            "com.netease.lztgglobal",
+            "com.pubg.imobile",
+            "com.pubg.krmobile",
+            "com.rekoo.pubgm",
+            "com.riotgames.league.wildrift",
+            "com.riotgames.league.wildrifttw",
+            "com.riotgames.league.wildriftvn",
+            "com.tencent.ig",
+            "com.tencent.tmgp.pubgmhd",
+            "com.vng.pubgmobile"
+    };
+
+    private static final Map<String, Object> propsToChangeOP9P = createMap("LE2101", "OnePlus");
+    private static final String[] packagesToChangeOP9P = { // spoof as OnePlus 9 PRO
+            "com.epicgames.fortnite",
+            "com.epicgames.portal",
+            "com.tencent.lolm"
+    };
+
+    private static final Map<String, Object> propsToChangeF5 = createMap("23049PCD8G", "Xiaomi");
+    private static final String[] packagesToChangeF5 = { // spoof as POCO F5
+            "com.dts.freefiremax",
+            "com.dts.freefireth"
+    };
+
+    private static final Map<String, Object> propsToChangeROG6 = createMap("ASUS_AI2201", "asus");
+    private static final String[] packagesToChangeROG6 = { // spoof as ROG Phone 6
+            "com.activision.callofduty.shooter",
+    	    "com.ea.gp.fifamobile",
+            "com.gameloft.android.ANMP.GloftA9HM",
+            "com.madfingergames.legends",
+            "com.pearlabyss.blackdesertm",
+            "com.pearlabyss.blackdesertm.gl"
+    };
+
+    private static final Map<String, Object> propsToChangeXP5 = createMap("SO-52A", "Sony");
+    private static final String[] packagesToChangeXP5 = { // spoof as Xperia 5
+            "com.garena.game.codm",
+            "com.tencent.tmgp.kr.codm",
+            "com.vng.codmvn"
+    };
+
+    private static Map<String, Object> createMap(String model, String manufacturer) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("MODEL", model);
+        map.put("MANUFACTURER", manufacturer);
+        return map;
+    }
+
+    public static void setProps(String packageName) {
+        if (packageName == null || packageName.isEmpty()) {
+            return;
+        }
+        Map<String, Object> propsToChange = null;
+        if (!SystemProperties.getBoolean("persist.sys.pixelprops.games", false)) {
+            return;
+        } else {
+            if (Arrays.asList(packagesToChangeBS4).contains(packageName)) {
+                propsToChange = propsToChangeBS4;
+            } else if (Arrays.asList(packagesToChangeMI11TP).contains(packageName)) {
+                propsToChange = propsToChangeMI11TP;
+            } else if (Arrays.asList(packagesToChangeMI13P).contains(packageName)) {
+                propsToChange = propsToChangeMI13P;
+            } else if (Arrays.asList(packagesToChangeOP8P).contains(packageName)) {
+                propsToChange = propsToChangeOP8P;
+            } else if (Arrays.asList(packagesToChangeOP9P).contains(packageName)) {
+                propsToChange = propsToChangeOP9P;
+            } else if (Arrays.asList(packagesToChangeF5).contains(packageName)) {
+                propsToChange = propsToChangeF5;
+            } else if (Arrays.asList(packagesToChangeROG6).contains(packageName)) {
+                propsToChange = propsToChangeROG6;
+            } else if (Arrays.asList(packagesToChangeXP5).contains(packageName)) {
+                propsToChange = propsToChangeXP5;
+            }
+        }
+        if (propsToChange != null) {
+            dlog("Defining props for: " + packageName);
+            for (Map.Entry<String, Object> prop : propsToChange.entrySet()) {
+                String key = prop.getKey();
+                Object value = prop.getValue();
+                setPropValue(key, value);
+            }
+        }
+    }
+
+    private static void setPropValue(String key, Object value) {
+        try {
+            dlog("Defining prop " + key + " to " + value.toString());
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
+    public static void dlog(String msg) {
+        if (DEBUG) Log.d(TAG, msg);
+    }
+}

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,6 +45,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.pixys.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -174,6 +175,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {

StagOS/android_frameworks_base

core/java/android/app/Instrumentation.java
@@ -59,6 +59,8 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.stag.PixelPropsUtils;
+import com.android.internal.util.stag.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1281,6 +1283,9 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1298,6 +1303,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
 

core/java/com/android/internal/util/stag/GamesPropsUtils.java
@@ -1,0 +1,155 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.stag;
+
+import android.os.Build;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GamesPropsUtils {
+
+    private static final String TAG = GamesPropsUtils.class.getSimpleName();
+    private static final boolean DEBUG = false;
+
+    private static final Map<String, Object> propsToChangeBS4 = createMap("2SM-X706B", "blackshark");
+    private static final String[] packagesToChangeBS4 = { // spoof as Black Shark 4
+            "com.proximabeta.mf.uamo"
+    };
+
+    private static final Map<String, Object> propsToChangeMI11TP = createMap("2107113SI", "Xiaomi");
+    private static final String[] packagesToChangeMI11TP = { // spoof as Mi 11T PRO
+            "com.ea.gp.apexlegendsmobilefps",
+            "com.levelinfinite.hotta.gp",
+            "com.supercell.clashofclans",
+            "com.vng.mlbbvn",
+            "com.mobile.legends"
+    };
+
+    private static final Map<String, Object> propsToChangeMI13P = createMap("2210132C", "Xiaomi");
+    private static final String[] packagesToChangeMI13P = { // spoof as Mi 13 PRO
+            "com.levelinfinite.sgameGlobal",
+            "com.tencent.tmgp.sgame"
+    };
+
+    private static final Map<String, Object> propsToChangeOP8P = createMap("IN2020", "OnePlus");
+    private static final String[] packagesToChangeOP8P = { // spoof as OnePlus 8 PRO
+            "com.netease.lztgglobal",
+            "com.pubg.imobile",
+            "com.pubg.krmobile",
+            "com.rekoo.pubgm",
+            "com.riotgames.league.wildrift",
+            "com.riotgames.league.wildrifttw",
+            "com.riotgames.league.wildriftvn",
+            "com.tencent.ig",
+            "com.tencent.tmgp.pubgmhd",
+            "com.vng.pubgmobile"
+    };
+
+    private static final Map<String, Object> propsToChangeOP9P = createMap("LE2101", "OnePlus");
+    private static final String[] packagesToChangeOP9P = { // spoof as OnePlus 9 PRO
+            "com.epicgames.fortnite",
+            "com.epicgames.portal",
+            "com.tencent.lolm"
+    };
+
+    private static final Map<String, Object> propsToChangeF5 = createMap("23049PCD8G", "Xiaomi");
+    private static final String[] packagesToChangeF5 = { // spoof as POCO F5
+            "com.dts.freefiremax",
+            "com.dts.freefireth"
+    };
+
+    private static final Map<String, Object> propsToChangeROG6 = createMap("ASUS_AI2201", "asus");
+    private static final String[] packagesToChangeROG6 = { // spoof as ROG Phone 6
+            "com.activision.callofduty.shooter",
+    	    "com.ea.gp.fifamobile",
+            "com.gameloft.android.ANMP.GloftA9HM",
+            "com.madfingergames.legends",
+            "com.pearlabyss.blackdesertm",
+            "com.pearlabyss.blackdesertm.gl"
+    };
+
+    private static final Map<String, Object> propsToChangeXP5 = createMap("SO-52A", "Sony");
+    private static final String[] packagesToChangeXP5 = { // spoof as Xperia 5
+            "com.garena.game.codm",
+            "com.tencent.tmgp.kr.codm",
+            "com.vng.codmvn"
+    };
+
+    private static Map<String, Object> createMap(String model, String manufacturer) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("MODEL", model);
+        map.put("MANUFACTURER", manufacturer);
+        return map;
+    }
+
+    public static void setProps(String packageName) {
+        if (packageName == null || packageName.isEmpty()) {
+            return;
+        }
+        Map<String, Object> propsToChange = null;
+        if (!SystemProperties.getBoolean("persist.sys.pixelprops.games", false)) {
+            return;
+        } else {
+            if (Arrays.asList(packagesToChangeBS4).contains(packageName)) {
+                propsToChange = propsToChangeBS4;
+            } else if (Arrays.asList(packagesToChangeMI11TP).contains(packageName)) {
+                propsToChange = propsToChangeMI11TP;
+            } else if (Arrays.asList(packagesToChangeMI13P).contains(packageName)) {
+                propsToChange = propsToChangeMI13P;
+            } else if (Arrays.asList(packagesToChangeOP8P).contains(packageName)) {
+                propsToChange = propsToChangeOP8P;
+            } else if (Arrays.asList(packagesToChangeOP9P).contains(packageName)) {
+                propsToChange = propsToChangeOP9P;
+            } else if (Arrays.asList(packagesToChangeF5).contains(packageName)) {
+                propsToChange = propsToChangeF5;
+            } else if (Arrays.asList(packagesToChangeROG6).contains(packageName)) {
+                propsToChange = propsToChangeROG6;
+            } else if (Arrays.asList(packagesToChangeXP5).contains(packageName)) {
+                propsToChange = propsToChangeXP5;
+            }
+        }
+        if (propsToChange != null) {
+            dlog("Defining props for: " + packageName);
+            for (Map.Entry<String, Object> prop : propsToChange.entrySet()) {
+                String key = prop.getKey();
+                Object value = prop.getValue();
+                setPropValue(key, value);
+            }
+        }
+    }
+
+    private static void setPropValue(String key, Object value) {
+        try {
+            dlog("Defining prop " + key + " to " + value.toString());
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
+    public static void dlog(String msg) {
+        if (DEBUG) Log.d(TAG, msg);
+    }
+}

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,6 +45,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.stag.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -174,6 +175,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {