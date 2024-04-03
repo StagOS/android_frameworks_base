ee3de9d7251f3df3dc85a958deae45d65c37a81f
Karan Parashar <karan@pixelos.net>
2024-04-03 18:45:17 +0200

    PropImitationHooks: Update fingerprints for May release
    Change-Id: Id678be96febf39e11c98aed7445b7fb9bc4a2330

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -83,7 +83,7 @@ public class PropImitationHooks {
         "MANUFACTURER", "Google",
         "BRAND", "google",
         "MODEL", "Pixel 8 Pro",
-        "FINGERPRINT", "google/husky/husky:14/AP1A.240305.019.A1/11445699:user/release-keys"
+        "FINGERPRINT", "google/husky/husky:14/AP1A.240505.005/11677807:user/release-keys"
     );
 
     private static final Map<String, String> sPixelOneProps = Map.of(
@@ -101,7 +101,7 @@ public class PropImitationHooks {
         "MANUFACTURER", "Google",
         "BRAND", "google",
         "MODEL", "Pixel Tablet",
-        "FINGERPRINT", "google/tangorpro/tangorpro:14/AP1A.240305.019.A1/11445699:user/release-keys"
+        "FINGERPRINT", "google/tangorpro/tangorpro:14/AP1A.240505.004/11583682:user/release-keys"
     );
 
     private static final Set<String> sPixelFeatures = Set.of(
