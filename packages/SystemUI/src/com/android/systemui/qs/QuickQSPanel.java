81c5e7ebc7ad3dd11754867d9a4725a5dfe25681
vjspranav <pranavasri@live.in>
2024-06-29 15:13:04 +0200

    Update default values of brightness slider * I Like it always visible and at the bottom

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -160,7 +160,7 @@ public class QuickQSPanel extends QSPanel implements TunerService.Tunable {
         switch (key) {
             case QS_SHOW_BRIGHTNESS_SLIDER:
                 boolean value =
-                        TunerService.parseInteger(newValue, 1) > 1;
+                        TunerService.parseInteger(newValue, 2) > 1;
                 super.onTuningChanged(key, value ? newValue : "0");
                 break;
             case QS_LAYOUT_COLUMNS:
